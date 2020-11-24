2311c31cac3b7cdfdfd34e252032bd5c98420083
tonyketcham <ketcham@pnw.edu>
2020-11-24 10:55:38 -0600

    Light wording changes

--- src/cms/index.js
+++ src/cms/index.js
@@ -2,7 +2,7 @@ import CMS from 'netlify-cms';
 import { youtubeControl, youtubePreview } from 'netlify-cms-widget-youtube';
 import { IdControl, IdPreview } from './id-widget';
 
-console.log('Init Custom CMS');
+console.log('Welcome to tea town brother');
 // Register the widget. This lets NetlifyCMS know about our custom widget
 CMS.registerWidget('youtube', youtubeControl, youtubePreview);
 
