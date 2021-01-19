433973f0e62465ab9924efd25422e37d8273a953
tonyketcham <ketcham@pnw.edu>
2021-01-19 02:27:13 -0600

    Project config overhaul

---
+++ stylelint.config.js
@@ -0,0 +1,20 @@
+// https://www.meidev.co/blog/visual-studio-code-css-linting-with-tailwind/
+module.exports = {
+  extends: ['stylelint-config-recommended'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
+    'declaration-block-trailing-semicolon': null,
+    'no-descending-specificity': null,
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,34 +3,15 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
-    content: [
-      './src/**/*.html',
-      './src/**/*.vue',
-      './src/**/*.jsx',
-    ]
+    content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
   },
   darkMode: 'class', // manually trigger
   theme: {
-    colors: {
-      gray: colors.blueGray,
-      red: colors.red,
-      orange: colors.orange,
-      yellow: colors.amber,
-      green: colors.lime,
-      teal: colors.teal,
-      cyan: colors.cyan,
-      indigo: colors.indigo,
-      purple: colors.purple,
-      pink: colors.pink,
-      rose: colors.rose,
-      white: colors.white
-    },
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      strokeWidth: ['hover', 'focus'],
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+};
