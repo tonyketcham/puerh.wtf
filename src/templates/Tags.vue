d570733beb734109e9517284b216a64db454a33b
Tony Ketcham <ketcham@pnw.edu>
2021-01-31 09:24:43 -0600

    Add Tags taxonomy page

---
+++ src/templates/Tags.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-full h-full p-6 overflow-y-scroll">
+    <h1 class="text-6xl">{{ $page.tags.title }}</h1>
+  </div>
+</template>
+
+<page-query>
+query Tags ($id: ID!) {
+  tags(id: $id) {
+        id
+        title
+        color
+    }
+}
+</page-query>
+
+<script>
+  export default {};
+</script>
