58999fc01407ae997ca9dde6587ce386b2fd8df3
Tony Ketcham <tonyketcham@gmail.com>
2021-01-31 00:13:11 -0600

    Create modular header component

---
+++ src/components/header/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header
+    class="fixed w-full p-4 transition-colors duration-150 ease-in-out bg-gray-200 border-b border-gray-300 shadow h-14 bg-opacity-40 border-opacity-20 dark:bg-gray-800 dark:border-gray-700"
+    style="backdrop-filter: blur(4px)"
+  >
+    <nav class="flex w-full m-auto md:px-6 place-content-between">
+      <div class="1/2 md:w-2/6">
+        <g-link
+          to="/"
+          class="font-mono text-xl font-medium tracking-tight transition duration-200 ease-in-out transform scale-95 hover:text-yellow-800 dark:hover:text-yellow-300 -rotate-1 hover:scale-100"
+          >🌱 {{ $static.metadata.siteName }} 🍵</g-link
+        >
+      </div>
+
+      <div class="flex-1">
+        <Nav-Head />
+      </div>
+
+      <div class="flex justify-end w-2/6">
+        <Theme-Toggle />
+      </div>
+    </nav>
+  </header>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+  import NavHead from './modules/Nav-Head.vue';
+  import ThemeToggle from './modules/Theme-Toggle.vue';
+  export default {
+    components: { ThemeToggle, NavHead },
+  };
+</script>
---
+++ src/components/header/modules/Nav-Head.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="justify-center hidden space-x-6 md:flex">
+    <g-link v-for="link in links" :key="link.title" :to="link.to" class="link">
+      {{ link.title }}
+    </g-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        links: [
+          { title: 'Sessions', to: '/sessions/' },
+          { title: 'Categories', to: '/categories/' },
+          { title: 'Vendors', to: '/vendors/' },
+          { title: 'About', to: '/about/' },
+        ],
+      };
+    },
+  };
+</script>
+
+<style scoped>
+  .link {
+    @apply text-gray-500 dark:text-gray-400 transition-all duration-500 ease-out;
+  }
+  .link:hover {
+    @apply text-gray-600 dark:text-gray-300;
+  }
+  .active {
+    @apply dark:text-white text-black;
+  }
+</style>
