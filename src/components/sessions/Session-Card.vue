723988cb77fe31eda4a0b4d7f2d00a7baca3c6a7
Tony Ketcham <ketcham@pnw.edu>
2021-02-02 23:36:52 -0600

    Float the tag badge set

--- src/components/sessions/Session-Card.vue
+++ src/components/sessions/Session-Card.vue
@@ -1,16 +1,16 @@
 <template>
   <div
-    class="flex justify-between h-56 overflow-hidden rounded-lg rim border-opacity-20 bg-gray-50 dark:bg-gray-700"
-    :style="`border-left: solid 3.5px ${color}`"
+    class="relative flex justify-between h-64 overflow-hidden rounded shadow-md rim bg-gray-50 dark:bg-gray-700"
+    :style="`border-left: solid 6px ${color}`"
   >
-    <div class="flex flex-col w-1/2 h-full md:w-2/3">
-      <simplebar class="m-2 mb-0 mr-0 h-9">
-        <div class="h-6 flex flex-nowrap space-x-1.5">
-          <TagBadge v-for="tag in info.tags" :key="tag.title" :tag="tag" />
-        </div>
-      </simplebar>
-      <div class="m-4">
-        <h2 class="text-2xl md:text-3xl">
+    <div class="flex flex-col w-1/2 h-full lg:w-7/12">
+      <!-- <simplebar class="absolute z-30 m-2 mb-0 mr-0 h-9"> -->
+      <!-- <div class="h-6 flex flex-nowrap space-x-1.5">
+        <TagBadge v-for="tag in info.tags" :key="tag.title" :tag="tag" />
+      </div> -->
+      <!-- </simplebar> -->
+      <div class="m-4 leading-5">
+        <h2 class="mb-4 text-2xl font-medium md:text-4xl dark:text-gray-50">
           <g-link :to="info.path">
             {{ info.title }}
           </g-link>
@@ -20,28 +20,32 @@
         </p>
       </div>
     </div>
-    <div class="w-1/2 h-full overflow-hidden shadow-md md:w-1/3">
-      <img
-        v-if="info.images[0]"
-        :src="info.images[0].image"
-        :alt="info.images[0].alt"
-        class="object-cover w-full h-full"
-      />
-      <div v-else class="flex w-full h-full bg-white">
+    <div class="relative w-1/2 h-full overflow-hidden lg:w-5/12">
+      <div v-if="info.images[0]">
+        <img
+          :src="info.images[0].image"
+          :alt="info.images[0].alt"
+          class="absolute object-cover w-full h-full"
+        />
+      </div>
+      <div v-else class="absolute flex w-full h-full bg-white">
         <h3 class="m-auto text-gray-600">Coming Soon...</h3>
       </div>
+    </div>
+    <div class="absolute bottom-3 right-3 h-6 flex flex-nowrap space-x-1.5">
+      <TagBadge v-for="tag in info.tags" :key="tag.title" :tag="tag" />
     </div>
   </div>
 </template>
 
 <script>
   import TagBadge from '../tags/Tag-Badge.vue';
-  import simplebar from 'simplebar-vue';
+  // import simplebar from 'simplebar-vue';
   import 'simplebar/dist/simplebar.min.css';
   export default {
     components: {
       TagBadge,
-      simplebar,
+      // simplebar,
     },
     props: {
       info: Object,
