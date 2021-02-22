a6cd7d0f701acedb260bb474bff71424219a67eb
Tony Ketcham <ketcham@pnw.edu>
2021-02-22 01:17:46 -0600

    Migrate Tailwind to Windi CSS

--- gridsome.config.js
+++ gridsome.config.js
@@ -35,13 +35,30 @@ module.exports = {
       ],
     },
   },
+  configureWebpack: (config) => {
+    config.module.rules.push({
+      test: /\.vue$/,
+      use: [
+        {
+          loader: 'vue-windicss-preprocess',
+          options: {
+            config: 'tailwind.config.js', // tailwind config file path OR config object (optional)
+            compile: false, // false: interpretation mode; true: compilation mode
+            globalPreflight: true, // set preflight style is global or scoped
+            globalUtility: true, // set utility style is global or scoped
+            prefix: 'windi-', // set compilation mode style prefix
+          },
+        },
+      ],
+    });
+  },
   plugins: [
-    {
-      use: 'gridsome-plugin-tailwindcss',
-      options: {
-        tailwindConfig: './tailwind.config.js',
-      },
-    },
+    // {
+    //   use: 'gridsome-plugin-tailwindcss',
+    //   options: {
+    //     tailwindConfig: './tailwind.config.js',
+    //   },
+    // },
     {
       use: '@gridsome/source-filesystem',
       options: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,42 @@
+const colors = require('windicss/colors');
+
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
-  },
   darkMode: 'class', // manually trigger
+  purge: {
+    options: {
+      enabled: true,
+      keyframes: true,
+      content: ['./public/index.html', './src/**/*.vue'],
+      // considers dynamic class bindings when purging unused classes
+      // credit: https://github.com/matebek https://dev.to/matebek
+      defaultExtractor: (content) => [
+        ...(content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []),
+        ...(content.match(/(?<=class:)[^=>/\s]*/g) || []),
+      ],
+    },
+  },
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      green: colors.lime,
+      teal: colors.teal,
+      cyan: colors.cyan,
+    },
     extend: {
       width: {
         112: '28rem',
       },
     },
   },
-  variants: {
-    extend: {
-      strokeWidth: ['hover', 'focus'],
-    },
-  },
   plugins: [
-    require('tailwindcss-textshadow'),
-    require('tailwind-filter-utilities'),
+    // require('windicss/plugin/typography'),
+    // require('windicss/plugin/filters'),
   ],
 };
