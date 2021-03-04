e5ec7a13b81b3c0414cbc89df499ffc047206977
Tony Ketcham <tonyketcham@gmail.com>
2021-03-04 12:57:28 -0600

    feat(layout): header is now sticky instead of fixed

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,10 +1,10 @@
 <template>
   <header
-    class="fixed w-full p-4 transition-colors duration-200 ease-in-out bg-gray-200 border-b border-gray-300 shadow h-14 bg-opacity-40 border-opacity-20 dark:bg-gray-800 dark:border-gray-700"
+    class="sticky w-full py-4 transition-colors duration-200 ease-in-out bg-gray-200 border-b border-gray-300 shadow h-14 bg-opacity-40 border-opacity-20 dark:bg-gray-800 dark:border-gray-700"
     style="backdrop-filter: blur(4px)"
   >
-    <nav class="flex w-full m-auto md:px-6 place-content-between">
-      <div class="1/2 md:w-2/6">
+    <nav class="flex w-full px-3 m-auto place-content-between">
+      <div class="lg:w-2/6">
         <g-link
           to="/"
           class="font-mono text-xl font-medium tracking-tight transition duration-200 ease-in-out transform scale-95 hover:text-yellow-800 dark:hover:text-yellow-300 -rotate-1 hover:scale-100"
@@ -16,7 +16,7 @@
         <Nav-Head />
       </div>
 
-      <div class="flex justify-end w-2/6">
+      <div class="flex justify-end lg:w-2/6">
         <Theme-Toggle />
       </div>
     </nav>
