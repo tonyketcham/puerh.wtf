57fa4b0b4b9ed022f558a4b3e619e6cf7d880921
Tony Ketcham <ketcham@pnw.edu>
2021-03-11 19:21:38 -0600

    feat(core): color palette changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
@@ -5,6 +7,20 @@ module.exports = {
   },
   darkMode: 'class', // manually trigger
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: colors.white,
+      black: colors.black,
+      gray: colors.warmGray,
+      red: colors.red,
+      orange: colors.orange,
+      yellow: colors.amber,
+      green: colors.lime,
+      blue: colors.cyan,
+      indigo: colors.indigo,
+      pink: colors.rose,
+    },
     extend: {
       width: {
         112: '28rem',
