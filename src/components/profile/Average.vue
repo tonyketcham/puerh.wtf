3d0968a7164ac97f2d6d3b6529c1e2e00d95ee3f
Tony Ketcham <tonyketcham@gmail.com>
2021-06-09 10:36:29 -0500

    add(component): average functional component

---
+++ src/components/profile/Average.vue
@@ -0,0 +1,22 @@
+<template>
+  <span>
+    <slot :value="averageYear(years)">
+      {{ averageYear(years) }}
+    </slot>
+  </span>
+</template>
+
+<script>
+  import { average } from '@/lib/reducers/arithmetic.js';
+
+  export default {
+    props: {
+      years: Array,
+    },
+    methods: {
+      averageYear(years) {
+        return Math.round(average(years));
+      },
+    },
+  };
+</script>
