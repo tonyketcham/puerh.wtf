ec22bceb03c27075602f0f4d84be7bb80863d5a0
Tony Ketcham <tonyketcham@gmail.com>
2021-06-09 10:13:26 -0500

    register new fonts/styles at app root

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,12 @@
 </template>
 
 <static-query>
-query {
-  metadata {
-    siteName
-    siteDescription
+  query {
+    metadata {
+      siteName
+      siteDescription
+    }
   }
-}
 </static-query>
 
 <script>
@@ -36,9 +36,23 @@ query {
   };
 </script>
 
-<style>
+<style lang="postcss">
   /* Hide scrollbar for Chrome, Safari and Opera */
   /* ::-webkit-scrollbar {
     @apply hidden;
   } */
+  @font-face {
+    font-family: 'Lexend';
+    src: local('Lexend'),
+      url('../static/fonts/Lexend/Lexend.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'Caveat';
+    src: local('Caveat'),
+      url('../static/fonts/Caveat/Caveat-VariableFont_wght.woff2')
+        format('woff2');
+    font-variation-settings: 'wght' 375;
+  }
+
+  /* font-variation-settings: 'wght' 375, 'GRAD' 88; */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import VueLazyload from 'vue-lazyload';
+import '~/styles/Global.postcss';
 
 export default function (Vue) {
   // Set default layout as a global component
