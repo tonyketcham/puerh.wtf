8f05ad46d98885291ee14c5fce44be59aceb3263
Tony Ketcham <tonyketcham@gmail.com>
2021-06-09 10:34:39 -0500

    fix(component): componentalize logo

---
+++ src/components/brand/Logo.vue
@@ -0,0 +1,54 @@
+<template>
+  <g-link to="/" class="inline-block font-cursive">
+    <div
+      @mouseenter="changeFontWeight(700)"
+      @mouseleave="changeFontWeight(400)"
+      :class="`text-${intensitySwitch}xl`"
+      class="transform group uniform-transition max-w-max"
+      :style="{ 'font-variation-settings': `'wght' ${fontWeight}` }"
+    >
+      <h1 class="inline-block">🍵 {{ $static.metadata.siteName }}</h1>
+      <span
+        class="inline-block transition-transform transform  group-hover:scale-150 group-hover:translate-x-1 group-hover:rotate-3"
+        >?</span
+      >
+    </div>
+  </g-link>
+</template>
+
+<static-query>
+  query {
+    metadata {
+      siteName
+    }
+  }
+</static-query>
+
+<script>
+  export default {
+    props: {
+      intensity: {
+        type: Number,
+        default: 3,
+      },
+    },
+
+    computed: {
+      intensitySwitch() {
+        return this.intensity === 1 ? '' : this.intensity;
+      },
+    },
+
+    data() {
+      return {
+        fontWeight: 50,
+      };
+    },
+
+    methods: {
+      changeFontWeight(wght) {
+        this.fontWeight = wght;
+      },
+    },
+  };
+</script>
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,41 +1,28 @@
 <template>
-  <header
-    class="sticky w-full py-4 transition-colors duration-200 ease-in-out bg-gray-200 border-b border-gray-300 shadow h-14 bg-opacity-40 border-opacity-20 dark:bg-gray-800 dark:border-gray-700"
-    style="backdrop-filter: blur(4px)"
-  >
-    <nav class="flex w-full px-3 m-auto place-content-between">
-      <div class="lg:w-2/6">
-        <g-link
-          to="/"
-          class="font-mono text-xl font-medium tracking-tight transition duration-200 ease-in-out transform scale-95 hover:text-yellow-800 dark:hover:text-yellow-300 -rotate-1 hover:scale-100"
-          >🌱 {{ $static.metadata.siteName }} 🍵</g-link
-        >
+  <header class="sticky px-1 py-4 m-2" style="width: calc(100% - 1rem)">
+    <nav class="flex px-3 m-auto place-content-between">
+      <div class="w-60">
+        <Logo />
       </div>
 
-      <div class="flex-1">
+      <div class="my-auto">
         <Nav-Head />
       </div>
 
-      <div class="flex justify-end lg:w-2/6">
+      <div class="flex justify-end flex-1">
         <Theme-Toggle />
       </div>
     </nav>
   </header>
 </template>
 
-<static-query>
-query {
-  metadata {
-    siteName
-  }
-}
-</static-query>
-
 <script>
+  import Logo from '../brand/Logo.vue';
   export default {
     components: {
       ThemeToggle: () => import('./modules/Theme-Toggle.vue'),
       NavHead: () => import('./modules/Nav-Head.vue'),
+      Logo,
     },
   };
 </script>
