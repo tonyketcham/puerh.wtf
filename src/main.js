0246deb525f99adc9fc88bdcd3b07124438cba84
Tony Ketcham <ketcham@pnw.edu>
2021-08-08 01:13:24 -0500

    add(core): VueTippy registration

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import '~/styles/Global.postcss';
 import Layout from '~/layouts/Default.vue';
+
+// Vue Tippy - tooltips
+import VueTippy, { TippyComponent } from 'vue-tippy';
+
+// import smoothscroll from 'smoothscroll-polyfill';
+// smoothscroll.polyfill();
 export default function (Vue) {
+  // Layout
   Vue.component('Layout', Layout);
+
+  // Tooltips
+  Vue.use(VueTippy, {
+    placement: 'left',
+    arrow: true,
+    boundary: 'viewport',
+    duration: 200,
+    delay: [0, 20],
+    inertia: true,
+    size: 'small',
+  });
+  Vue.component('tippy', TippyComponent);
 }
