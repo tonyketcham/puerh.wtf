46ac9d5ee6511bd846103268457b27bccef59b11
Tony Ketcham <tonyketcham@gmail.com>
2021-08-23 01:18:58 -0500

    fix(ui/ux): increase card cohesion

--- src/components/sessions/Card.vue
+++ src/components/sessions/Card.vue
@@ -1,64 +1,55 @@
 <template>
   <div
-    class="relative flex-shrink-0 overflow-hidden group rounded-2xl w-52 h-96"
+    class="relative flex-shrink-0 h-full overflow-hidden border-2 border-gray-800  group rounded-2xl w-52 max-h-96"
     style="scroll-snap-align: center"
   >
     <g-link :to="session.path">
-      <g-image
-        :alt="session.title"
-        :src="session.images[0].image"
-        class="absolute inset-0 object-cover w-full h-full"
-      />
+      <div class="absolute inset-0 w-full h-full">
+        <g-image
+          :alt="session.title"
+          :src="session.images[0].image"
+          blur="80"
+          width="208"
+          height="384"
+          class="object-cover w-full h-full"
+        />
+      </div>
     </g-link>
     <div
-      class="absolute bottom-0 left-0 right-0 w-full pointer-events-none  h-2/3 opacity-90 bg-gradient-to-t from-black to-transparent group-hover:h-3/4"
+      class="absolute bottom-0 left-0 right-0 w-full pointer-events-none  h-2/3 bg-gradient-to-t from-black to-transparent group-hover:h-3/4"
     ></div>
-    <!-- <g-link :to="session.vendor[0].path">
-      <g-image
-        class="absolute top-0 right-0 object-cover object-center w-8 h-8 m-2 transition-all duration-200 ease-in-out rounded-full opacity-60 filter blur-sm hover:blur-none ring-0 hover:ring-2 ring-white hover:opacity-10"
-        :src="session.vendor[0].image"
-        :alt="`Vendor: ${session.vendor[0].title}`"
-        v-tippy
-        :content="`from ${session.vendor[0].title}`"
-      />
-      <g-image
-        class="absolute top-0 right-0 object-cover object-center w-8 h-8 m-2 rounded-full pointer-events-none "
-        :src="session.vendor[0].image"
-        :alt="`Vendor: ${session.vendor[0].title}`"
-        v-tippy
-        :content="`${session.vendor[0].title}`"
-      />
-    </g-link> -->
-    <div class="absolute bottom-0 left-0 right-0 p-2">
-      <div
-        class="h-6 text-xs leading-7 text-gray-200 max-w-max opacity-60"
-        style="font-variation-settings: 'LXND' 7"
+    <div class="absolute bottom-0 left-0 right-0 flex flex-col p-2">
+      <span
+        class="text-xl max-w-max text-shadow-xl"
+        style="font-variation-settings: 'wght' 600, 'LXND' 16"
+        :style="`color: ${startColor};`"
       >
-        <g-link :to="session.vendor[0].path">
-          <span>{{ session.vendor[0].title }}</span>
-        </g-link>
-      </div>
-      <div>
-        <span
-          class="text-xl max-w-max"
-          style="font-variation-settings: 'wght' 700"
-          :style="`color: ${startColor};`"
-          >{{ session.production_year }}</span
-        >
-        <span class="mx-1 text-gray-200 select-none opacity-40">•</span>
-        <span
-          class="text-sm text-gray-200 lowercase align-text-top  opacity-60 max-w-max"
-          style="font-variation-settings: 'wght' 450, 'LXND' 14"
-          >{{ session.style[0].category.title }}</span
-        >
-      </div>
+        {{ session.production_year }}
+      </span>
       <g-link :to="session.path">
         <h2 class="text-2xl font-semibold text-gray-50 line-clamp-2">
           {{ session.title }}
         </h2>
       </g-link>
-      <aside class="hidden text-sm text-gray-400 group-hover:block">
-        <p>{{ session.excerpt }}</p>
+      <div
+        class="inline text-xs leading-7 text-gray-800 max-w-max"
+        style="font-variation-settings: 'LXND' 7"
+      >
+        <g-link :to="session.vendor[0].path">
+          <span class="bg-primary-light text-padding">{{
+            session.vendor[0].title
+          }}</span>
+        </g-link>
+      </div>
+      <span
+        class="text-sm text-gray-800 lowercase align-text-top  max-w-max bg-primary-light text-padding"
+        style="font-variation-settings: 'wght' 450, 'LXND' 14"
+        >{{ session.style[0].category.title }}</span
+      >
+      <aside class="hidden text-sm text-gray-800 group-hover:block">
+        <p class="inline bg-primary-light max-w-max text-padding">
+          {{ session.excerpt }}
+        </p>
       </aside>
     </div>
   </div>
@@ -84,3 +75,9 @@
     },
   };
 </script>
+
+<style>
+  .text-padding {
+    box-shadow: -7px 0 0 rgba(254, 244, 232), 7px 0 0 rgba(254, 244, 232);
+  }
+</style>
